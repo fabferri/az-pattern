b242d97be3b43d4d7f600f2b036e0b3c07e30463
fabferri <fabferri@outlook.com>
2019-08-22 21:16:19 +0100

    Azure Examples

--- 00-scripts/scripts/enableipv6.sh
+++ 00-scripts/scripts/enableipv6.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 # file: /etc/sysconfig/network  entry: NETWORKING_IPV6=yes
-sed -i \
-    -e '/^\(NETWORKING_IPV6=\).*/{s//\11/;:a;n;ba;q}' \
-    -e '$aNETWORKING_IPV6=yes' /etc/sysconfig/network
+sed -i -e '/^\(NETWORKING_IPV6=\).*/{s//\11/;:a;n;ba;q}' -e '$aNETWORKING_IPV6=yes' /etc/sysconfig/network
 
 # file: /etc/sysconfig/network-scripts/ifcfg-eth0  entry: IPV6INIT=yes
-sed -i \
-    -e '/^\(IPV6INIT=\).*/{s//\11/;:a;n;ba;q}' \
-    -e '$aIPV6INIT=yes' /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i -e '/^\(IPV6INIT=\).*/{s//\11/;:a;n;ba;q}' -e '$aIPV6INIT=yes' /etc/sysconfig/network-scripts/ifcfg-eth0
 
 # file: /etc/sysconfig/network-scripts/ifcfg-eth0  entry: DHCPV6C=yes
-sed -i \
-    -e '/^\(DHCPV6C=\).*/{s//\12/;:a;n;ba;q}' \
-    -e '$aDHCPV6C=yes' /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i -e '/^\(DHCPV6C=\).*/{s//\12/;:a;n;ba;q}' -e '$aDHCPV6C=yes' /etc/sysconfig/network-scripts/ifcfg-eth0
 
 # disable and reable the interface to get the new setup
 ifdown eth0 && ifup eth0
