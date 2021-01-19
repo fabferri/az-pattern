76775213d011f0b829d3c9981ca7f535e427562b
fabferri <fabferri@outlook.com>
2021-01-19 16:44:29 +0000

    Azure examples

---
+++ 00-basic-examples/00-singleVM/single-vm.ps1
@@ -0,0 +1,65 @@
+# Before running the script set the variables:
+#
+#   $adminUsername    : administrator username. please replace ADMINISTRATOR_USER with your username before running the script!!
+#   $adminPassword    : adminsitrator password. please replace ADMINISTRATOR_PASSWORD with your password before running the script!!
+#   $subscriptionName : name of the Azure subscription
+#   $location         : Azure region when deployed the resource group
+#   $rgName           : resource group name
+#   $armTemplateFile  : ARM template file
+#   tags:
+#       $RGTagExpireDate : expire date
+#       $RGTagContact    : contact email
+#       $RGTagUsage      : deployment purpose
+#
+#
+[CmdletBinding()]
+param (
+    [Parameter( Mandatory = $false, ValueFromPipeline=$false, HelpMessage='username administrator VMs')]
+    [string]$adminUsername = "ADMINISTRATOR_USER",
+ 
+    [Parameter(Mandatory = $false, HelpMessage='password administrator VMs')]
+    [string]$adminPassword = "ADMINISTRATOR_PASSWORD"
+    )
+
+################# Input parameters #################
+$subscriptionName = "AzDemo" 
+$location         = "eastus"
+$rgName           = "vm-dev001"
+$deploymentName   = "single-vm"
+$armTemplateFile  = "single-vm.json"
+##
+## tags
+$RGTagExpireDate =((Get-Date).AddMonths(1)).ToString('yyyy-MM-dd')
+$RGTagContact = 'user1@contoso.com' 
+$RGTagAlias = 'user1' 
+$RGTagUsage = 'dev VM' 
+
+####################################################
+
+$pathFiles      = Split-Path -Parent $PSCommandPath
+$templateFile   = "$pathFiles\$armTemplateFile"
+
+$parameters=@{
+              "adminUsername"= $adminUsername;
+              "adminPassword"= $adminPassword
+              }
+
+$subscr=Get-AzSubscription -SubscriptionName $subscriptionName
+Select-AzSubscription -SubscriptionId $subscr.Id
+
+# Create Resource Group 
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Creating Resource Group $rgName " -ForegroundColor Cyan
+Try {$rg = Get-AzResourceGroup -Name $rgName  -ErrorAction Stop
+     Write-Host '  resource exists, skipping'}
+Catch {$rg = New-AzResourceGroup -Name $rgName  -Location $location  
+             Set-AzResourceGroup -Name $rgName `
+             -Tag @{Expires=$RGTagExpireDate; Contacts=$RGTagContact; Owner=$RGTagAlias; Usage=$RGTagUsage} | Out-Null
+}
+
+$runTime=Measure-Command {
+   write-host "running ARM template:"$templateFile
+   New-AzResourceGroupDeployment  -Name $deploymentName -ResourceGroupName $rgName -TemplateFile $templateFile -TemplateParameterObject $parameters -Verbose 
+}
+
+write-host "runtime: "$runTime.ToString() -ForegroundColor Yellow 
