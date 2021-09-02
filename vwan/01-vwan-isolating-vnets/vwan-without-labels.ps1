7c17203bb6fc5d03153c8af7c9e9cef00dc7f194
fabferri <fabferri@outlook.com>
2021-09-02 16:20:15 +0100

    azure examples

---
+++ vwan/01-vwan-isolating-vnets/vwan-without-labels.ps1
@@ -0,0 +1,123 @@
+#
+# The script read the init.json file and define the variables:
+#   $adminUsername
+#   $adminPassword
+#   $subscriptionName
+#   $ResourceGroupName
+#   $hub1_location
+#   $hub2_location
+#   $hub1Name
+#   $hub2Name
+#   $mngIP
+#   $RGTagExpireDate
+#   $RGTagContact
+#   $RGTagNinja
+#   $RGTagUsage
+#
+################# Input parameters #################
+$deploymentName = 'vwan1'
+$armTemplateFile = 'vwan-without-labels.json'
+$inputParams = 'init.json'
+####################################################
+
+$pathFiles  = Split-Path -Parent $PSCommandPath
+$templateFile = "$pathFiles\$armTemplateFile"
+
+$pathFiles = Split-Path -Parent $PSCommandPath
+$templateFile = "$pathFiles\$armTemplateFile"
+
+# reading the input parameter file $inputParams and convert the values in hashtable 
+If (Test-Path -Path $pathFiles\$inputParams) 
+{
+     # convert the json into PSCustomObject
+     $jsonObj = Get-Content -Raw $pathFiles\$inputParams | ConvertFrom-Json
+     if ($null -eq $jsonObj) {
+          Write-Host "file $inputParams is empty"
+          Exit
+     }
+     # convert the PSCustomObject in hashtable
+     if ($jsonObj -is [psobject]) {
+          $hash = @{}
+          foreach ($property in $jsonObj.PSObject.Properties) {
+               $hash[$property.Name] = $property.Value
+          }
+     }
+     foreach ($key in $hash.keys) {
+          $message = '{0} = {1} ' -f $key, $hash[$key]
+          Write-Output $message
+          Try { New-Variable -Name $key -Value $hash[$key] -ErrorAction Stop }
+          Catch { Set-Variable -Name $key -Value $hash[$key] }
+     }
+} 
+else { Write-Warning "$inputParams file not found, please change to the directory where these scripts reside ($pathFiles) and ensure this file is present."; Return }
+
+# checking the values of variables
+Write-Host "$(Get-Date) - values from file: $inputParams" -ForegroundColor Yellow
+if (!$adminUsername) { Write-Host 'variable $adminUsername is null' ; Exit } else { Write-Host '   administrator username: '$adminUsername -ForegroundColor Green}
+if (!$adminPassword) { Write-Host 'variable $adminPassword is null' ; Exit } else { Write-Host '   administrator password: '$adminPassword -ForegroundColor Green}
+if (!$subscriptionName) { Write-Host 'variable $subscriptionName is null' ; Exit } else { Write-Host '   subscription name..: '$subscriptionName -ForegroundColor Yellow}
+if (!$ResourceGroupName) { Write-Host 'variable $ResourceGroupName is null' ; Exit } else { Write-Host '   resource group name: '$ResourceGroupName -ForegroundColor Yellow}
+if (!$hub1_location) { Write-Host 'variable $hub1_location is null' ; Exit } else { Write-Host '   hub1 location...........: '$hub1_location -ForegroundColor Yellow}
+if (!$hub2_location) { Write-Host 'variable $hub2_location is null' ; Exit } else { Write-Host '   hub2 location...........: '$hub2_location -ForegroundColor Yellow}
+if (!$hub1Name) { Write-Host 'variable $hub1Name is null' ; Exit } else { Write-Host '   hub1 name...............: '$hub1Name -ForegroundColor Yellow}
+if (!$hub2Name) { Write-Host 'variable $hub2Name is null' ; Exit } else { Write-Host '   hub2 name...............: '$hub2Name -ForegroundColor Yellow}
+if (!$mngIP) { Write-Host 'variable $mngIP is null' ; Exit } else { Write-Host '   mngIP..............: '$mngIP -ForegroundColor Yellow}
+if (!$RGTagExpireDate) { Write-Host 'variable $RGTagExpireDate is null' ; Exit } else { Write-Host '   RGTagExpireDate....: '$RGTagExpireDate -ForegroundColor Yellow}
+if (!$RGTagContact) { Write-Host 'variable $RGTagContact is null' ; Exit } else { Write-Host '   RGTagContact.......: '$RGTagContact -ForegroundColor Yellow}
+if (!$RGTagNinja) { Write-Host 'variable $RGTagNinja is null' ; Exit } else { Write-Host '   RGTagNinja.........: '$RGTagNinja -ForegroundColor Yellow}
+if (!$RGTagUsage) { Write-Host 'variable $RGTagUsage is null' ; Exit } else { Write-Host '   RGTagUsage.........: '$RGTagUsage -ForegroundColor Yellow}
+$rgName=$ResourceGroupName
+
+
+
+$subscr=Get-AzSubscription -SubscriptionName $subscriptionName
+Select-AzSubscription -SubscriptionId $subscr.Id
+
+# Login Check
+Try {Write-Host 'Using Subscription: ' -NoNewline
+     Write-Host $((Get-AzContext).Name) -ForegroundColor Green}
+Catch {
+    Write-Warning 'You are not logged in dummy. Login and try again!'
+    Return}
+
+
+$parameters=@{
+              "mngIP"= $mngIP;
+              "hub1_location"= $hub1_location;
+              "hub2_location"= $hub2_location;
+              "hub1Name"= $hub1Name;
+              "hub2Name"= $hub2Name;
+              "adminUsername"= $adminUsername;
+              "adminPassword"= $adminPassword
+              }
+
+# Create Resource Group
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host 'Creating Resource Group' -ForegroundColor Cyan
+Try { Get-AzResourceGroup -Name $rgName -ErrorAction Stop
+     Write-Host 'Resource exists, skipping'}
+Catch { New-AzResourceGroup -Name $rgName -Location $hub1_location}
+
+# set a tag on the resource group if it doesn't exist.
+if ((Get-AzResourceGroup -Name $rgName).Tags -eq $null)
+{
+  # Add Tag Values to the Resource Group
+  Set-AzResourceGroup -Name $rgName -Tag @{Expires=$RGTagExpireDate; Contacts=$RGTagContact; Pathfinder=$RGTagNinja; Usage=$RGTagUsage} | Out-Null
+}
+
+$startTime = "$(Get-Date)"
+$runTime=Measure-Command {
+   write-host "running ARM template:"$templateFile
+   New-AzResourceGroupDeployment  -Name $deploymentName -ResourceGroupName $rgName -TemplateFile $templateFile -TemplateParameterObject $parameters -Verbose 
+}
+ 
+write-host "runtime...: "$runTime.ToString() -ForegroundColor Yellow
+write-host "start time: "$startTime -ForegroundColor Yellow
+write-host "endt time.: "$(Get-Date) -ForegroundColor Yellow
+
+
+
+
+
+
+
