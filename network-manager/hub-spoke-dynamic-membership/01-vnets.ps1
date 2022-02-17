6850551ae447fbab7b9b6989721ecd9545296c05
fabferri <fabferri@outlook.com>
2022-02-17 21:47:19 +0000

    azure virtual network manager

---
+++ network-manager/hub-spoke-dynamic-membership/01-vnets.ps1
@@ -0,0 +1,178 @@
+# Before running the script, customize the value of the variables in the init.json file
+#
+################# Input parameters #################
+$deploymentName = "anm-vnets"
+$armTemplateFile = "01-vnets.json"
+$inputParams = 'init.json'
+####################################################
+
+$pathFiles = Split-Path -Parent $PSCommandPath
+$templateFile = "$pathFiles\$armTemplateFile"
+
+# reading the input parameter file $inputParams and convert the values in hashtable 
+If (Test-Path -Path $pathFiles\$inputParams) {
+    # convert the json into PSCustomObject
+    $jsonObj = Get-Content -Raw $pathFiles\$inputParams | ConvertFrom-Json
+    if ($null -eq $jsonObj) {
+        Write-Host "file $inputParams is empty"
+        Exit
+    }
+    # convert the PSCustomObject in hashtable
+    if ($jsonObj -is [psobject]) {
+        $hash = @{}
+        foreach ($property in $jsonObj.PSObject.Properties) {
+            $hash[$property.Name] = $property.Value
+        }
+    }
+    foreach ($key in $hash.keys) {
+        $message = '{0} = {1} ' -f $key, $hash[$key]
+        Write-Output $message
+        Try { New-Variable -Name $key -Value $hash[$key] -ErrorAction Stop }
+        Catch { Set-Variable -Name $key -Value $hash[$key] }
+    }
+} 
+else { Write-Warning "$inputParams file not found, please change to the directory where these scripts reside ($pathFiles) and ensure this file is present."; Return }
+
+# checking the values of variables
+Write-Host "$(Get-Date) - values from file: $inputParams" -ForegroundColor Yellow
+if (!$adminUsername) { Write-Host 'variable $adminUsername is null' ; Exit }         else { Write-Host '   administrator username: '$adminUsername -ForegroundColor Green }
+if (!$adminPassword) { Write-Host 'variable $adminPassword is null' ; Exit }         else { Write-Host '   administrator password: '$adminPassword -ForegroundColor Green }
+if (!$subscriptionName) { Write-Host 'variable $subscriptionName is null' ; Exit }   else { Write-Host '   subscription name.....: '$subscriptionName -ForegroundColor Yellow }
+if (!$location) { Write-Host 'variable $location is null' ; Exit }                   else { Write-Host '   location..............: '$location -ForegroundColor Yellow }
+if (!$locationhub) { Write-Host 'variable $locationhub is null' ; Exit }             else { Write-Host '   locationhub...........: '$locationhub -ForegroundColor Yellow }
+if (!$location1) { Write-Host 'variable $location1 is null' ; Exit }                 else { Write-Host '   location1.............: '$location1 -ForegroundColor Yellow }
+if (!$location2) { Write-Host 'variable $location2 is null' ; Exit }                 else { Write-Host '   location2.............: '$location2 -ForegroundColor Yellow }
+if (!$location3) { Write-Host 'variable $location3 is null' ; Exit }                 else { Write-Host '   location3.............: '$location3 -ForegroundColor Yellow }
+if (!$location4) { Write-Host 'variable $location4 is null' ; Exit }                 else { Write-Host '   location4.............: '$location4 -ForegroundColor Yellow }
+if (!$resourceGroupName) { Write-Host 'variable $resourceGroupName is null' ; Exit }               else { Write-Host '   resource group name........: '$resourceGroupName -ForegroundColor Yellow }
+if (!$resourceGroupNameHubVNet) { Write-Host 'variable $resourceGroupNameHubVNet is null' ; Exit } else { Write-Host '   resourceGroupNameHubVNet...: '$resourceGroupNameHubVNet -ForegroundColor Yellow }
+if (!$resourceGroupNameVNet1) { Write-Host 'variable $resourceGroupNameVNet1 is null' ; Exit }     else { Write-Host '   resourceGroupNameVNet1.....: '$resourceGroupNameVNet1 -ForegroundColor Yellow }
+if (!$resourceGroupNameVNet2) { Write-Host 'variable $resourceGroupNameVNet2 is null' ; Exit }     else { Write-Host '   resourceGroupNameVNet2.....: '$resourceGroupNameVNet2 -ForegroundColor Yellow }
+if (!$resourceGroupNameVNet3) { Write-Host 'variable $resourceGroupNameVNet3 is null' ; Exit }     else { Write-Host '   resourceGroupNameVNet3.....: '$resourceGroupNameVNet3 -ForegroundColor Yellow }
+if (!$resourceGroupNameVNet4) { Write-Host 'variable $resourceGroupNameVNet1 is null' ; Exit }     else { Write-Host '   resourceGroupNameVNet4.....: '$resourceGroupNameVNet4 -ForegroundColor Yellow }
+if (!$mngIP) { Write-Host 'variable $mngIP is null' ; Exit }                         else { Write-Host '   mngIP.................: '$mngIP -ForegroundColor Yellow }
+if (!$RGTagExpireDate) { Write-Host 'variable $RGTagExpireDate is null' ; Exit }     else { Write-Host '   RGTagExpireDate.......: '$RGTagExpireDate -ForegroundColor Yellow }
+if (!$RGTagContact) { Write-Host 'variable $RGTagContact is null' ; Exit }           else { Write-Host '   RGTagContact..........: '$RGTagContact -ForegroundColor Yellow }
+if (!$RGTagNinja) { Write-Host 'variable $RGTagNinja is null' ; Exit }               else { Write-Host '   RGTagNinja............: '$RGTagNinja -ForegroundColor Yellow }
+if (!$RGTagUsage) { Write-Host 'variable $RGTagUsage is null' ; Exit }               else { Write-Host '   RGTagUsage............: '$RGTagUsage -ForegroundColor Yellow }
+$rgName = $ResourceGroupName
+
+$subscr = Get-AzSubscription -SubscriptionName $subscriptionName
+Select-AzSubscription -SubscriptionId $subscr.Id
+
+# Login Check
+Try {
+    Write-Host 'Using Subscription: ' -NoNewline
+    Write-Host $((Get-AzContext).Name) -ForegroundColor Green
+}
+Catch {
+    Write-Warning 'You are not logged in dummy. Login and try again!'
+    Return
+}
+
+$subscr = Get-AzSubscription -SubscriptionName $subscriptionName
+Select-AzSubscription -SubscriptionId $subscr.Id
+
+
+# Create Resource Group for the hub vnet
+Write-Host "$(Get-Date) - Creating Resource Group $resourceGroupNameHubVNet " -ForegroundColor Cyan
+Try {
+    $rg = Get-AzResourceGroup -Name $resourceGroupNameHubVNet  -ErrorAction Stop
+    Write-Host '  resource exists, skipping'
+    Set-AzResourceGroup -Name $resourceGroupNameHubVNet -Tag @{Environment = "SHARED" }
+}
+Catch {
+    $rg = New-AzResourceGroup -Name $resourceGroupNameHubVNet  -Location $locationhub  
+    Set-AzResourceGroup -Name $resourceGroupNameHubVNet -Tag @{Environment = "SHARED" } 
+}
+
+# Create Resource Group for the vnet1
+Write-Host "$(Get-Date) - Creating Resource Group $resourceGroupNameVNet1 " -ForegroundColor Cyan
+Try {
+    $rg = Get-AzResourceGroup -Name $resourceGroupNameVNet1  -ErrorAction Stop
+    Write-Host '  resource exists, skipping'
+    Set-AzResourceGroup -Name $resourceGroupNameVNet1 -Tag @{Environment = "PROD" } 
+}
+Catch {
+    $rg = New-AzResourceGroup -Name $resourceGroupNameVNet1  -Location $location1  
+    Set-AzResourceGroup -Name $resourceGroupNameVNet1 -Tag @{Environment = "PROD" } 
+}
+
+# Create Resource Group for the vnet2
+Write-Host "$(Get-Date) - Creating Resource Group $resourceGroupNameVNet2 " -ForegroundColor Cyan
+Try {
+    $rg = Get-AzResourceGroup -Name $resourceGroupNameVNet2  -ErrorAction Stop
+    Write-Host '  resource exists, skipping'
+    Set-AzResourceGroup -Name $resourceGroupNameVNet2 -Tag @{Environment = "PROD" } 
+}
+Catch {
+    $rg = New-AzResourceGroup -Name $resourceGroupNameVNet2  -Location $location2  
+    Set-AzResourceGroup -Name $resourceGroupNameVNet2 -Tag @{Environment = "PROD" } 
+}
+
+# Create Resource Group for the vnet3
+Write-Host "$(Get-Date) - Creating Resource Group $resourceGroupNameVNet3 " -ForegroundColor Cyan
+Try {
+    $rg = Get-AzResourceGroup -Name $resourceGroupNameVNet3  -ErrorAction Stop
+    Write-Host '  resource exists, skipping'
+    Set-AzResourceGroup -Name $resourceGroupNameVNet3 -Tag @{Environment = "TEST" } 
+}
+Catch {
+    $rg = New-AzResourceGroup -Name $resourceGroupNameVNet3  -Location $location3  
+    Set-AzResourceGroup -Name $resourceGroupNameVNet3 -Tag @{Environment = "TEST" } 
+}
+
+# Create Resource Group for the vnet4
+Write-Host "$(Get-Date) - Creating Resource Group $resourceGroupNameVNet4 " -ForegroundColor Cyan
+Try {
+    $rg = Get-AzResourceGroup -Name $resourceGroupNameVNet4  -ErrorAction Stop
+    Write-Host '  resource exists, skipping'
+    Set-AzResourceGroup -Name $resourceGroupNameVNet4 -Tag @{Environment = "TEST" } 
+}
+Catch {
+    $rg = New-AzResourceGroup -Name $resourceGroupNameVNet4  -Location $location4  
+    Set-AzResourceGroup -Name $resourceGroupNameVNet4 -Tag @{Environment = "TEST" } 
+}
+
+
+# Create Resource Group 
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Creating Resource Group $rgName " -ForegroundColor Cyan
+Try {
+    $rg = Get-AzResourceGroup -Name $rgName  -ErrorAction Stop
+    Write-Host '  resource exists, skipping'
+}
+Catch {
+    $rg = New-AzResourceGroup -Name $rgName  -Location $location  
+    Set-AzResourceGroup -Name $rgName `
+        -Tag @{Expires = $RGTagExpireDate; Contacts = $RGTagContact; Owner = $RGTagAlias; Usage = $RGTagUsage } | Out-Null
+}
+
+$parameters = @{ 
+    "resourceGroupNameHubVNet" = $resourceGroupNameHubVNet;
+    "resourceGroupNameVNet1" = $resourceGroupNameVNet1;
+    "resourceGroupNameVNet2" = $resourceGroupNameVNet2;
+    "resourceGroupNameVNet3" = $resourceGroupNameVNet3;
+    "resourceGroupNameVNet4" = $resourceGroupNameVNet4;
+    "locationhub" = $locationhub;
+    "location1" = $location1;
+    "location2" = $location2;
+    "location3" = $location3;
+    "location4" = $location4
+}
+
+$startTime = Get-Date
+$runTime = Measure-Command {
+    write-host "$startTime - running ARM template:"$templateFile
+    New-AzResourceGroupDeployment  -Name $deploymentName -ResourceGroupName $rgName -TemplateFile $templateFile -TemplateParameterObject $parameters -Verbose 
+}
+
+# End and printout the runtime
+$endTime = Get-Date
+$TimeDiff = New-TimeSpan $startTime $endTime
+$Mins = $TimeDiff.Minutes
+$Secs = $TimeDiff.Seconds
+$RunTime = '{0:00}:{1:00} (M:S)' -f $Mins, $Secs
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Script completed" -ForegroundColor Green
+Write-Host "  Time to complete: $RunTime" -ForegroundColor Yellow
---
+++ network-manager/hub-spoke-dynamic-membership/README.md
@@ -0,0 +1,136 @@
+<properties
+pageTitle= 'Create a hub-spoke network topology with dynamic membership in Azure Virtual Network Manager'
+description= "Create a hub-spoke network topology with dynamic membership in Azure Virtual Network Manager"
+documentationcenter: na
+services="Azure Virtual Network Manager"
+documentationCenter="na"
+authors="fabferri"
+manager=""
+editor=""/>
+
+<tags
+   ms.service="configuration-Example-Azure"
+   ms.devlang="na"
+   ms.topic="article"
+   ms.tgt_pltfrm="azure"
+   ms.workload="na"
+   ms.date="02/17/2022"
+   ms.author="fabferri" />
+
+# Create a hub-spoke network topology with dynamic membership in Azure Virtual Network Manager
+In this article is described a simple hub-spoke topology with dynamic membership to the network groups. The network diagram is shown below:
+
+[![1]][1]
+
+<br>
+
+The following communications are allow/denied:
+|        | vnet1 | vnet2 | vnet3 | vnet4 | hub   |    
+| ------ | ----- | ----- | ----- | ----- | ----- |
+| vnet1  |   -   | allow | deny  | deny  | allow |
+| vnet2  | allow |  -    | deny  | deny  | allow |
+| vnet3  | deny  | deny  |   -   | allow | allow |
+| vnet4  | deny  | deny  | allow |   -   | allow |
+| hub    | allow | allow | allow | allow |   -   |
+
+
+**NOTE**: 
+- the **networkManagerScopeAccesses** is set only to **"Connectivity"**; the ARM template does not use security admin rules
+- the network manager is configured in hub-spoke topology
+- the network manager is scoped to a single Azure subscription
+- five different VNets are created in different resource groups; each resource group has a specific tag 
+- the network groups use dynamic membership; conditional statements are used to include in network group VNets deployed in Azure Resource Groups
+- the connectivity configuration is configured with _enable connectivity within each network group_
+- two network groups are defined named **grp1** and **grp2**
+- the network group **grp1** with dynamic membership includes all the VNets for "PROD" environment
+- the network group **grp2** with dynamic membership includes all the VNets for "TEST" environment
+
+To dynamic membership for the network group **grp1** is established with the following structure:  
+```json
+"{ 
+        \"allOf\": [
+            {
+                \"value\": \"[resourceGroup().Name]\", 
+                \"contains\": \"PROD\" 
+            }
+        ]
+}"
+```
+
+The string of dynamic membership can be along in single line, by the control character <b>\n</b> , but the format is less readable:
+
+```json
+"{ \n   \"allOf\": [\n   {\n    \"value\": \"[resourceGroup().Name]\", \n   \"contains\": \"PROD\" \n   }\n   ]\n  }"
+```
+
+The dynamic membership is shown in the Azure management portal as follow:
+
+[![2]][2]
+
+By the dynamic membership described, it is possible to include in dynamic membership all the VNets, in the Azure subscription, deployed in resource groups which name contains "PROD".
+
+<br>
+
+## <a name="List of files"></a> List of files 
+
+| file                    | description                                                        |       
+| ----------------------- |:------------------------------------------------------------------ |
+| **01-vnets.json**       | ARM template to create 5 VNets in different resource groups        |
+| **01-vnets.ps1**        | powershell script to deploy the ARM template **01-vnets.json**     |
+| **02-vms.json**         | ARM template to create the VMs                                     |
+| **02-vms.ps1**          | powershell script to deploy the ARM template **02-vms.json**       |
+| **03-anm.json**         | ARM template to create network groups and a network connectivity configuration |
+| **03-anm.ps1**          | powershell script to deploy the ARM template **03-anm.json**       |
+
+<br>
+
+Before spinning up the powershell scripts, **01-vnets-vms.ps1** and **02-anm.ps1**, you have to edit the file **init.json** and customize the values of variables:
+The structure of **init.json** file is shown below:
+```json
+{
+    "adminUsername": "ADMINISTRATOR_USERNAME",
+    "adminPassword": "ADMINISTRATOR_PASSWORD",
+    "subscriptionName": "NAME_AZURE_SUBSCRIPTION",
+    "resourceGroupName": "anm",
+    "resourceGroupNameHubVNet": "anm-SHARED",
+    "resourceGroupNameVNet1": "anm-PROD",
+    "resourceGroupNameVNet2": "anm-PROD",
+    "resourceGroupNameVNet3": "anm-TEST",
+    "resourceGroupNameVNet4": "anm-TEST",
+    "location": "westus2",
+    "locationhub": "westus2",
+    "location1": "westus2",
+    "location2": "westus2",
+    "location3": "westus2",
+    "location4": "westus2",
+    "mngIP": "MANAGEMENT_PUBLIC_IP_ADDRESS_TO_CONNECT_IN_SSH_TO_THE_VMs",
+    "RGTagExpireDate": "03/17/2022",
+    "RGTagContact": "user1@contoso.com",
+    "RGTagNinja": "user1",
+    "RGTagUsage": "test azure network manager"
+}
+```
+<br>
+
+Deployment requires to run the scripts in sequence (respect the order of execution otherwise the scripts will fail):
+- step 1: edit the **init.json** file to customize the values of variables  
+- step 2: run the powershell script **01-vnets.ps1**; this creates the resource groups and VNets 
+- step 3: run the powershell script **02-vms.ps1**; this creates the VMs in same resource groups of the related VNets.
+- step 4: run the powershell script **03-anm.ps1** 
+- step 5: deployment of the network connectivity configuration you have created in a specific Azure region through Azure management portal
+
+<br>
+
+Full network diagram inclusive of IP networks and VMs:
+
+[![3]][3]
+
+
+<!--Image References-->
+
+[1]: ./media/network-diagram1.png "network diagram"
+[2]: ./media/network-diagram2.png "network diagram"
+[3]: ./media/network-diagram3.png "network diagram with IP networks"
+
+<!--Link References-->
+
