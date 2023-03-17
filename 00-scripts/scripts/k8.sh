beace3c0f976551f059d82c85ab748577277f817
fabferri <fabferri@outlook.com>
2023-03-17 08:46:34 +0000

    add k8.sh

--- 00-scripts/scripts/k8.sh
+++ 00-scripts/scripts/k8.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-# script to install kubernetes
+# script to install kubernetes in ubuntu
 #
-apt update
-apt install -y bash-completion
+apt-get update
+apt-get install -y bash-completion
 
 # disable swap
 sudo swapoff -a
@@ -27,24 +27,61 @@ EOF
 # reload the above changes
 sudo sysctl --system
 
-#  install containerd runtime
-sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+# grab the OS version
+OSVER=$(cat /etc/lsb-release | grep  DISTRIB_RELEASE | cut -d "=" -f2 )
 
-# in ubutu 20.04
-mkdir /etc/apt/keyrings
+if [ $OSVER = "20.04" ]
+then
+   # in ubutu 20.04
+   mkdir /etc/apt/keyrings
+fi
+
 #
 # GPG key used to verify the docker repo package 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+#  install containerd runtime
+sudo apt-get install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 # install containerd
-sudo apt update
-sudo apt install -y containerd.io
+sudo apt-get update
+sudo apt-get install -y containerd.io
 
-# configure containerd so that it starts using systemd as cgroup.
-containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
-sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+### configure containerd so that it starts using systemd as cgroup.
+# containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+#sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
+# the containerd version provided in Ubuntu repo is too old
+# replace with recent version
+sudo systemctl stop containerd
+
+# to install the last version of containerd
+# cleanup old files from previous attempt if existing
+if [ -d "/var/temp/" ];
+then
+	[ -d /var/temp/bin ] && rm -rf /var/temp/bin
+else
+  mkdir /var/temp/
+fi
+wget https://github.com/containerd/containerd/releases/download/v1.6.19/containerd-1.6.19-linux-amd64.tar.gz -P /var/temp/
+tar xzvf /var/temp/containerd-1.6.19-linux-amd64.tar.gz --directory /var/temp/
+sudo mv /var/temp/bin/* /usr/bin/
+
+# Configure containerd
+sudo mkdir -p /etc/containerd
+cat <<- TOML | sudo tee /etc/containerd/config.toml
+version = 2
+[plugins]
+  [plugins."io.containerd.grpc.v1.cri"]
+    [plugins."io.containerd.grpc.v1.cri".containerd]
+      discard_unpacked_layers = true
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+          runtime_type = "io.containerd.runc.v2"
+          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+            SystemdCgroup = true
+	TOML
 
 #  restart and enable containerd service
 sudo systemctl restart containerd
@@ -57,8 +94,8 @@ sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packag
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # install Kubernetes components Kubectl, kubeadm & kubelet
-sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 
