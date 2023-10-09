690ad642cdb1cdcad31da8cc1c93948e2b37a0a0
fabferri <fabferri@outlook.com>
2023-10-09 19:27:23 +0100

    ExpressRoute circuit connected to two vnets

--- expressroute/er-circuit-2vnets/2VNets-1ERcircuit.ps1
+++ expressroute/er-circuit-2vnets/2VNets-1ERcircuit.ps1
@@ -1,36 +1,38 @@
-#
-#
-[CmdletBinding()]
-param (
-    [Parameter( Mandatory = $false, ValueFromPipeline=$false, HelpMessage='username administrator VMs')]
-    [string]$adminUsername = "YOUR_ADMIN_USERNAME",
- 
-    [Parameter(Mandatory = $false, HelpMessage='password administrator VMs')]
-    [string]$adminPassword = "YOUR_ADMIN_PASSWORD"
-    )
-
 ################# Input parameters #################
-$subscriptionName      = "AzDev"     
-$location              = "northeurope"
-$rgName                = "1-test"
-$rgDeployment          = "dep01"
-$armTemplateFile       = "2VNets-1ERcircuit.json"
+$adminUsername = 'ADMINISTRATOR_USERNAME'
+$adminPassword = 'ADMINISTRATOR_PASSWORD'
+$subscriptionName = 'AzDev'   
+$location = 'westus2'  
+$rgName = '1-test'
+$deploymentName = 'dep01'
+$armTemplateFile = '2VNets-1ERcircuit.json'
 ####################################################
 
-$pathFiles      = Split-Path -Parent $PSCommandPath
-$templateFile   = "$pathFiles\$armTemplateFile"
-$parameters=@{
-              "adminUsername"= $adminUsername;
-              "adminPassword"= $adminPassword
-              }
-
-$subscr=Get-AzSubscription -SubscriptionName $subscriptionName
-Select-AzSubscription -SubscriptionId $subscr.Id
-
-$runTime=Measure-Command {
-New-AzResourceGroup -Name $rgName -Location $location
-write-host $templateFile
-New-AzResourceGroupDeployment  -Name $rgDeployment -ResourceGroupName $rgName -TemplateFile $templateFile -TemplateParameterObject $parameters -Verbose
+$pathFiles = Split-Path -Parent $PSCommandPath
+$templateFile = "$pathFiles\$armTemplateFile"
+$parameters = @{
+    "adminUsername" = $adminUsername;
+    "adminPassword" = $adminPassword
 }
 
-write-host -ForegroundColor Yellow "runtime: "$runTime.ToString()
+$subscr = Get-AzSubscription -SubscriptionName $subscriptionName
+Select-AzSubscription -SubscriptionId $subscr.Id
+
+# Create Resource Group 
+Write-Host "$(Get-Date) - Creating Resource Group $rgName " -ForegroundColor Cyan
+Try {
+  $rg = Get-AzResourceGroup -Name $rgName  -ErrorAction Stop
+  Write-Host '  resource exists, skipping'
+}
+Catch { $rg = New-AzResourceGroup -Name $rgName  -Location $location }
+
+$StartTime = Get-Date
+write-host "$StartTime - running ARM template: $templateFile"
+New-AzResourceGroupDeployment  -Name $deploymentName -ResourceGroupName $rgName -TemplateFile $templateFile -TemplateParameterObject $parameters -Verbose 
+
+$EndTime = Get-Date
+$TimeDiff = New-TimeSpan $StartTime $EndTime
+$Mins = $TimeDiff.Minutes
+$Secs = $TimeDiff.Seconds
+$RunTime = '{0:00}:{1:00} (M:S)' -f $Mins, $Secs
+Write-Host "runtime: $RunTime" -ForegroundColor Yellow
--- expressroute/er-circuit-2vnets/README.md
+++ expressroute/er-circuit-2vnets/README.md
@@ -34,15 +34,16 @@ The network diagram is shown below:
 
 > > [!NOTE]
 > Before spinning up the ARM template you should change the following in the file **2VNets-1ERcircuit.ps1**: <br>
-> parameter **azureRegion1**:  name of Azure region where is deployed the Azure VNet1 <br>
-> parameter **azureRegion2**:  name of Azure region where is deployed the Azure VNet2 <br>
-> **authorizationKey**:  authorization Key associated with the Expressroute circuit. Each Azure VNet requires a specific autorization key; you can't use the same authorization key to join more then one VNet to the same ExpressRoute circuit. <br>
+> parameter **location1**:  name of Azure region where is deployed the Azure VNet1 <br>
+> parameter **location2**:  name of Azure region where is deployed the Azure VNet2 <br>
+> **authorizationKey**:  authorization Key associated with the Expressroute circuit. Each ExpressRoute Connection requires a specific authorization key; you can't use the same authorization key to join more then one VNet to the same ExpressRoute circuit. <br>
 > **erCircuitId**: ExpressRoute circuit Id. The structure of Expressroute circuit Id is: **"/subscriptions/<subscription_ID>/resourceGroups/<resourceGroup_Name>/providers/Microsoft.Network/expressRouteCircuits/<ER_circuit_Name>"**
 > 
 
 
 `Tags: ExpressRoute`<br>
-`date: 10-08-22`
+`date: 10-08-22` <br>
+`date: 09-10-23`
 
 <!--Image References-->
 [1]: ./media/network-diagram.png "network diagram"
