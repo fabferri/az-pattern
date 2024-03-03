5243cbf0f2805791a44e81ddd471a2b5cdc32c59
fabferri <fabferri@outlook.com>
2024-03-03 18:39:04 +0000

    scripts

--- 00-scripts/scripts/CreateSelfSignCertificates.ps1
+++ 00-scripts/scripts/CreateSelfSignCertificates.ps1
@@ -46,8 +46,7 @@ $params = @{
        NotAfter = (Get-Date).AddMonths(18)
        CertStoreLocation = 'Cert:\CurrentUser\My'
        Signer = $certRoot
-       TextExtension = @(
-        '2.5.29.37={text}1.3.6.1.5.5.7.3.2')
+       TextExtension = @('2.5.29.37={text}1.3.6.1.5.5.7.3.2')
    }
 # Create client cert
 Write-Host "$(Get-Date) - Creating P2S Client cert"
