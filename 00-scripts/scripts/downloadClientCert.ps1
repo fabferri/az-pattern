0c8f709c0ba4fd3bf79ebd6fef8140e4589d8b15
fabferri <fabferri@outlook.com>
2024-03-14 21:07:49 +0000

    scripts

--- 00-scripts/scripts/downloadClientCert.ps1
+++ 00-scripts/scripts/downloadClientCert.ps1
@@ -1,10 +1,6 @@
 ### download the client certificate from storage account and install it in the Cert:\CurrentUser\My
 ###
 param(
-    [Parameter(Mandatory = $false, HelpMessage = 'administrator username', ValueFromPipeline = $true)]
-    [string]$adminUsername,
-    [Parameter(Mandatory = $false, HelpMessage = 'administrator password', ValueFromPipeline = $true)]
-    [string]$adminPassword,
     [Parameter(Mandatory = $false, HelpMessage = 'client certificate number values:[1,2,3,4,...]', ValueFromPipeline = $true)]
     [int]$clientCertSeq = 1
 )
@@ -13,8 +9,9 @@ if ($clientCertSeq -le 0) { $clientCertSeq = 1 }
 write-host 'number client certificates: '$clientCertSeq
 
 
-$clientCertFile = 'certClient'+ ([string]$clientCertSeq)+'.pfx'
+$clientCertFile = 'certClient' + ([string]$clientCertSeq) + '.pfx'
 $passwordCertFile = 'certpwd.txt'
+$vpnProfileFile = 'azurevpnconfig.xml'
 $certPath = 'C:\cert\'
 
 $pathFolder = [string](Split-Path -Path $certPath -Parent)
@@ -56,41 +53,68 @@ Write-Host ''
 Connect-AzAccount -Identity
 
 # get storage account name and resource group name
-$storageAccountName=(Get-AzStorageAccount).StorageAccountName
-$resourceGroupName=(Get-AzStorageAccount).ResourceGroupName
-$storageContext=(Get-AzStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroupName).Context
-$containerName=(Get-AzStorageContainer -Context $storageContext).Name
+$storageAccountName = (Get-AzStorageAccount).StorageAccountName
+$resourceGroupName = (Get-AzStorageAccount).ResourceGroupName
+$storageContext = (Get-AzStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroupName).Context
+$containerName = (Get-AzStorageContainer -Context $storageContext).Name
 
 
+try {
+    $clientCert = @{
+        Blob        = $clientCertFile
+        Container   = $containerName
+        Destination = "$pathFolder$folderName"
+        Context     = $storageContext
+    }
+    Get-AzStorageBlobContent @clientCert -Force -Verbose
+}
+catch { 
+    # the blob doesn't exist...
+    Write-Host "Blob $clientCertFile Not Found"
+}
 
-$clientCert = @{
-    Blob        = $clientCertFile
-    Container   = $containerName
-    Destination = "$pathFolder$folderName"
-    Context     = $storageContext
-  }
-Get-AzStorageBlobContent @clientCert -Force -Verbose
 
-$passwordCert = @{
-     Blob        = $passwordCertFile
-     Container   = $containerName
-     Destination = "$pathFolder$folderName"
-     Context     = $storageContext
-   }
-Get-AzStorageBlobContent @passwordCert -Force -Verbose
+try {
+    $passwordCert = @{
+        Blob        = $passwordCertFile
+        Container   = $containerName
+        Destination = "$pathFolder$folderName"
+        Context     = $storageContext
+    }
+    Get-AzStorageBlobContent $passwordCert -Force -Verbose
+}
+catch { 
+    # the blob doesn't exist...
+    Write-Host "Blob $passwordCertFile Not Found"
+}
+
+try {
+    $vpnProfile = @{
+        Blob        = $vpnProfileFile
+        Container   = $containerName
+        Destination = "$pathFolder$folderName"
+        Context     = $storageContext
+    }
+    Get-AzStorageBlobContent $vpnProfile -Force -Verbose
+}
+catch { 
+    # the blob doesn't exist...
+    Write-Host "Blob: $vpnProfileFile Not Found"
+}
+
 
 
 $fullPathCertClientFile = "$pathFolder$folderName\$clientCertFile"
 $fullPathPwdFile = "$pathFolder$folderName\$passwordCertFile"
 
-If (Test-Path -Path $fullPathCertClientFile){
-     write-host 'client certificate file: '$fullPathCertClientFile' found'
-   }
+If (Test-Path -Path $fullPathCertClientFile) {
+    write-host 'client certificate file: '$fullPathCertClientFile' found'
+}
 else { Write-Warning "$fullPathCertClientFile file not found, please change to the directory where these scripts reside ($pathFiles) and ensure this file is present."; Return }
 
-If (Test-Path -Path $fullPathPwdFile){
-     write-host 'certificate password file: '$fullPathPwdFile' found'
-   }
+If (Test-Path -Path $fullPathPwdFile) {
+    write-host 'certificate password file: '$fullPathPwdFile' found'
+}
 else { Write-Warning "$fullPathPwdFile file not found, please change to the directory where these scripts reside ($pathFiles) and ensure this file is present."; Return }
 
 # create a powershell scriptt add the client digital certificate 
