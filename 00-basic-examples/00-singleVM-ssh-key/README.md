f7a36e4e5554eb1da7f0a0de07bd875eb6a5e5f8
Fabrizio Ferri <fabferri@users.noreply.github.com>
2025-02-18 14:28:38 +0000

    azure examplea

---
+++ 00-basic-examples/00-singleVM-ssh-key/README.md
@@ -0,0 +1,79 @@
+<properties
+pageTitle= 'Linux VM with ssh key authetication and Azure Bastion'
+description= "Linux VM with ssh key authetication and Azure Bastion"
+documentationcenter: na
+services="Azure LinuxVM, SSH KEY"
+documentationCenter="[gitub](https://github.com/fabferri)"
+authors="fabferri"
+editor="fabferri"/>
+
+<tags
+   ms.service="configuration-Example-Azure"
+   ms.devlang="na"
+   ms.topic="Azure networking"
+   ms.tgt_pltfrm="Azure"
+   ms.workload="Azure LinuxVM with SSH key, Azure Bastion"
+   ms.date="18/01/2025"
+   ms.author="fabferri" />
+
+## Linux VM with ssh key authetication and Azure Bastion
+
+The ARM template creates a linux VM with ssh key authetication. In the Azure VNet is deployed Azure Bastion to connect to the VM. The network diagram is shown below: 
+
+[![1]][1]
+
+The  Linux VM has a public IP to connect in SSH to the VM without transit through the Azure Bastion. <br>
+Before running the script an SSH key needs to be generated. By git bash command:
+```bash
+ssh-keygen -m PEM -t rsa -b 4096  -f ./vmkey.pem
+```
+The ssh-keygen generated in local folder the private key named **vmkey.pem** in PEM format and a public key named **vmkey.pem.pub** <br>
+The file **init.json** contains a list of input variables. <br>
+```json
+{
+    "subscriptionName": "AZURE_SUBSCRIPTION",
+    "resourceGroupName": "RESOURCE_GROUP_NAME",
+    "location": "AZURE_REGION",
+    "adminUsername": "ADMINISTRATOR_USERNAME",
+    "adminPublicKey": "PUBLIC_SSH_KEY"
+}
+```
+
+Open the public key **vmkey.pem.pub** and paste the content in the variable **adminPublicKey** in **init.json**<br>
+After setting the variables in **init.json**, you can start the deployment through the script **vmsshkey.ps1**
+
+
+> [NOTE]
+>
+> if you want to add a comment to the public key you can use the command: <br>
+> <b>ssh-keygen -m PEM -t rsa -b 4096 -C "by-gitcommand" -f ./vmkey.pem</b> <br>
+>
+> -b flag determines the key length <br> 
+> -C provides a comment <br>
+>
+
+Since the public ssh key is part of the private ssh key, we can extract the public key from the private; to do this we use the ssh-keygen command:
+```bash
+ssh-keygen -y  -f ./vmkey.pem
+```
+-y : this option will read a private OpenSSH format file and print an OpenSSH public key to stdout. <br>
+
+To display the fingerprint and RCA length of specified public key file use the option -l: 
+```bash
+ssh-keygen -l  -f ./vmkey.pem.pub
+```
+
+To exact only the fingerprint:
+```bash
+ssh-keygen -l -f ./vmkey.pem.pub | cut -d ' ' -f 2
+```
+
+`Tag: SSH key` <br>
+`date: 18-02-2025`
+
+<!--Image References-->
+
+[1]: ./media/network-diagram.png "network diagram"
+
+<!--Link References-->
+
---
+++ 00-basic-examples/00-singleVM-ssh-key/vmsshkey.ps1
@@ -0,0 +1,71 @@
+################# Input parameters #################
+$deploymentName = 'vm-deployment'
+$armTemplateFile = 'vm-sshkey.json'
+$armTemplateParametersFile = 'init.json'
+####################################################
+
+$pathFiles = Split-Path -Parent $PSCommandPath
+$templateFile = "$pathFiles\$armTemplateFile"
+$parametersFile = "$pathFiles\$armTemplateParametersFile"
+
+try {
+    $arrayParams = (Get-Content -Raw $parametersFile | ConvertFrom-Json)
+    $subscriptionName = $arrayParams.subscriptionName
+    $resourceGroupName = $arrayParams.resourceGroupName
+    $location = $arrayParams.location
+    $adminUsername = $arrayParams.adminUsername
+    $adminPublicKey = $arrayParams.adminPublicKey
+    Write-Host "$(Get-Date) - values from file: "$parametersFile -ForegroundColor Yellow
+    if (!$subscriptionName) { Write-Host 'variable $subscriptionName is null' ; Exit }     else { Write-Host '   subscriptionName......: '$subscriptionName -ForegroundColor Yellow }
+    if (!$resourceGroupName) { Write-Host 'variable $resourceGroupName is null' ; Exit }   else { Write-Host '   resourceGroupName.....: '$resourceGroupName -ForegroundColor Yellow }
+    if (!$location) { Write-Host 'variable $location is null' ; Exit }                     else { Write-Host '   location..............: '$location -ForegroundColor Yellow }
+    if (!$adminUsername) { Write-Host 'variable $adminUsername is null' ; Exit }           else { Write-Host '   administrator username: '$adminUsername -ForegroundColor Green }
+    if (!$adminPublicKey) { Write-Host 'variable $adminPublicKey is null' ; Exit }         else { Write-Host '   admin adminPublicKey..: '$adminPublicKey -ForegroundColor Green }
+} 
+catch {
+    Write-Host 'error in reading the template file: '$parametersFile -ForegroundColor Yellow
+    Exit
+}
+
+$parameters = @{
+    "location"     = $location;
+    "adminUsername" = $adminUsername;
+    "adminPublicKey" = $adminPublicKey
+}
+
+# Login Check
+Try {
+    Write-Host 'Using Subscription: ' -NoNewline
+    Write-Host $((Get-AzContext).Name) -ForegroundColor Green
+}
+Catch {
+    Write-Warning 'You are not logged in dummy. Login and try again!'
+    Return
+}
+
+$subscr = Get-AzSubscription -SubscriptionName $subscriptionName
+Select-AzSubscription -SubscriptionId $subscr.Id
+
+# Create Resource Group 
+Write-Host "$(Get-Date) - Creating Resource Group: $resourceGroupName " -ForegroundColor Cyan
+Try {
+    $rg = Get-AzResourceGroup -Name $resourceGroupName  -ErrorAction Stop
+    Write-Host '  resource exists, skipping'
+}
+Catch {
+    $rg = New-AzResourceGroup -Name $resourceGroupName  -Location $location
+}
+
+$startTime = Get-Date
+write-host "$(Get-Date) - ARM template...: "$templateFile
+write-host "$(Get-Date) - parameters file: "$parametersFile
+
+New-AzResourceGroupDeployment  -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile $templateFile -TemplateParameterObject $parameters  -Verbose 
+Write-Host "$(Get-Date) - setup completed" -ForegroundColor Green
+$endTime = Get-Date
+$timeDiff = New-TimeSpan $startTime $endTime
+$mins = $timeDiff.Minutes
+$secs = $timeDiff.Seconds
+$runTime = '{0:00}:{1:00} (M:S)' -f $mins, $secs
+Write-Host "$(Get-Date) - Script completed" -ForegroundColor Green
+Write-Host "Time to complete: "$runTime -ForegroundColor Green
