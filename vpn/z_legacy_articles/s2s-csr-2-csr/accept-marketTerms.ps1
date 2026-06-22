e3e969152d3a47c2ed6698889e99ee3899821658
Fabrizio Ferri <fabferri@users.noreply.github.com>
2026-06-22 17:56:30 +0200

    removing stale projects

--- vpn/z_legacy_articles/s2s-csr-2-csr/accept-marketTerms.ps1
+++
@@ -1,3 +0,0 @@
-$agreementTerms=Get-AzMarketplaceTerms  -Publisher "cisco" -Product "cisco-csr-1000v"  -Name "17_3_4a-byol"
-
- Set-AzMarketplaceTerms -Publisher "cisco" -Product "cisco-csr-1000v" -Name "17_3_4a-byol" -Terms $agreementTerms -Accept
